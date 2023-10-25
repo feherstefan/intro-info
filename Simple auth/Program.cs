0c09548a58d77e641bfe731d40f804e53a7deb57
Stefan <73956050+feherstefan@users.noreply.github.com>
2023-10-25 23:55:40 +0300

    Update Program.cs
    refactored code after 3 years of experience

--- Simple auth/Program.cs
+++ Simple auth/Program.cs
@@ -1,105 +1,89 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
+    static List<User> users = new List<User>
+    {
+        new User("admin", "admin"),
+        new User("feherstefan", "jamasa123")
+    };
+
     static void Main(string[] args)
     {
-        var arrUsers = new Users[]
+        while (true)
         {
-           new Users("admin","admin"),
-           new Users("feherstefan","jamasa123")     
-        };
+            Console.WriteLine("1. Log in");
+            Console.WriteLine("2. Register");
+            Console.WriteLine("3. Exit");
+            var input = Console.ReadLine();
 
-    Start:
-        Console.WriteLine("1.Log in"); 
-        Console.WriteLine("2.Register");
-        var input = Console.ReadLine();
-
-
-
-        bool successfull = false;
-        while (!successfull)
-        {
-
-            if (input == "1")
+            switch (input)
             {
-                Console.WriteLine("Write your username:");
-                var username = Console.ReadLine();
-                Console.WriteLine("Enter your password:");
-                var password = Console.ReadLine();
-
-
-                foreach (Users user in arrUsers)
-                {
-                    if (username == user.username && password == user.password)
-                    {
-                        Console.WriteLine("You have successfully logged in !!!");
-                        Console.ReadLine();
-                        successfull = true;
-                        break;
-                    }
-                }
-
-                if (!successfull)
-                {
-                    Console.WriteLine("Your username or password is incorect, try again !!!");
-                }
-
+                case "1":
+                    Login();
+                    break;
+                case "2":
+                    Register();
+                    break;
+                case "3":
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
             }
-
-            else if (input == "2")
-            {
-
-                Console.WriteLine("Enter your username:");
-                var username = Console.ReadLine();
-
-                Console.WriteLine("Enter your password:");
-                var password = Console.ReadLine();
-
-                foreach (Users user in arrUsers)
-                {
-                    if (username == user.username && password == user.password)
-                    {
-                        Console.WriteLine("This account already exists.");
-                        Console.ReadLine();
-                        successfull = true;
-                        goto Start;
-                    }
-                }
-
-
-                Array.Resize(ref arrUsers, arrUsers.Length + 1);
-                arrUsers[arrUsers.Length - 1] = new Users(username, password);
-
-                successfull = true;
-                goto Start;
-
-            }
-            else
-            {
-                Console.WriteLine("Try again !!!");
-                break;
-
-
-            }
-
         }
-
     }
-}
 
-public  class Users
-{
-    public string username;
-    public string password;
-   
-
-    public  Users(string username, string password)
+    static void Login()
     {
-        this.username = username;
-        this.password = password;
-   
+        Console.WriteLine("Enter your username:");
+        var username = Console.ReadLine();
+        Console.WriteLine("Enter your password:");
+        var password = Console.ReadLine();
+
+        var user = users.FirstOrDefault(u => u.Username == username && u.Password == password);
+        if (user != null)
+        {
+            Console.WriteLine("You have successfully logged in!");
+        }
+        else
+        {
+            Console.WriteLine("Your username or password is incorrect. Please try again.");
+        }
+        Console.ReadLine();
+    }
+
+    static void Register()
+    {
+        Console.WriteLine("Enter your username:");
+        var username = Console.ReadLine();
+
+        var user = users.FirstOrDefault(u => u.Username == username);
+        if (user != null)
+        {
+            Console.WriteLine("This account already exists.");
+        }
+        else
+        {
+            Console.WriteLine("Enter your password:");
+            var password = Console.ReadLine();
+            users.Add(new User(username, password));
+            Console.WriteLine("Account created successfully.");
+        }
+        Console.ReadLine();
     }
 }
 
+public class User
+{
+    public string Username { get; }
+    public string Password { get; }
 
+    public User(string username, string password)
+    {
+        Username = username ?? throw new ArgumentNullException(nameof(username));
+        Password = password ?? throw new ArgumentNullException(nameof(password));
+    }
+}
